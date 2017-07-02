644300f26fc2ea311871a0ebfd6ba480ed9a8c6f
james <jamesdaniel2197@gmail.com>
2017-07-03 00:03:55 +0100

    update grid styles

--- src/components/grid.vue
+++ src/components/grid.vue
@@ -29,7 +29,6 @@
 
   @import "../scss/foundation.scss";
 
-
   $grid-gutter-small: 6px;
   $grid-gutter-medium: 2rem;
   $grid-gutter-large: 4rem;
@@ -71,15 +70,13 @@
     @include clearfix();
     background: $white;
     border-radius: 50%;
-    @include breakpoint(large) {
-      box-shadow: 0 2px 0 1px lighten($color-secondary, 12);
-    }
+    box-shadow: 0 2px 0 0 mix($white, $color-secondary);
   }
 
   .grid .kana__svg {
-    width: 40px;
-    height: 40px;
-    margin: 6px auto;
+    width: 32px;
+    height: 32px;
+    margin: 10px auto;
     transform: scale(1);
     transition: transform 0.2s ease;
     z-index: 0;
