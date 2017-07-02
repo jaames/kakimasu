94280e36dfc65a70791942254c56ebd9b2901790
james <jamesdaniel2197@gmail.com>
2017-07-02 19:50:15 +0100

    update template and poi config

--- poi.config.js
+++ poi.config.js
@@ -1,9 +1,26 @@
+try {
+  var privConfig = require("./config.private.js");
+}
+catch (e) {
+  if (e instanceof Error && e.code === "MODULE_NOT_FOUND")
+    var privConfig = {};
+  else
+    throw e;
+}
+
 module.exports = {
-  title: "Kakimasu",
   resolve: true,
-  template: "src/templates/index.html",
+  html: {
+    template: "./src/templates/index.html",
+    title: "Kakimasu",
+    baseURL: "https://kakimasu.rakujira.jp",
+    description: "Learn to write Japanese Hiragana and Katakana!",
+    creator: "@rakujira",
+    googleAnalytics: privConfig.googleAnalytics || false,
+    webfontFamilies: "Poppins:600:latin"
+  },
   mergeConfig: {
-    module: {loaders: [
+    module: { loaders: [
       { test: /\.json$/, loader: "json-loader" }
     ]},
   }
