ee71bd36cef971b0e2e2f09b0a219c9f5e0460a2
james <jamesdaniel2197@gmail.com>
2019-01-07 21:33:55 +0000

    port config to poi v12

--- poi.config.js
+++ poi.config.js
@@ -1,21 +1,25 @@
 var webpack = require("webpack");
 
+console.log(webpack);
+
 module.exports = {
-  resolve: true,
-  html: {
-    template: "./src/templates/index.html",
-    title: "Kakimasu",
-    baseURL: "https://kakimasu.rakujira.jp",
-    description: "Learn to write Japanese Hiragana and Katakana!",
-    creator: "@rakujira",
-    webfontFamilies: "Poppins:600:latin",
-    meta: {
-      "theme-color": "#88cdf1",
-      "msapplication-navbutton-color": "#88cdf1"
-    }
+  output: {
+    html: {
+      template: "./src/templates/index.html",
+      title: "Kakimasu",
+      baseURL: "https://kakimasu.rakujira.jp",
+      description: "Learn to write Japanese Hiragana and Katakana!",
+      creator: "@rakujira",
+      webfontFamilies: "Poppins:600:latin",
+      meta: {
+        "theme-color": "#88cdf1",
+        "msapplication-navbutton-color": "#88cdf1"
+      }
+    },
   },
-  webpack(config) {
-    config.module.loaders = [({ test: /\.json$/, loader: "json-loader" })];
+  configureWebpack(config) {
+    // as of webpack 2.0, webpack auto imports 
+    // config.module.rules.push({ test: /\.json$/, loader: "json-loader" });
     config.plugins.push(new webpack.BannerPlugin({
         banner: [
           "kakimasu.rakujira.jp",
